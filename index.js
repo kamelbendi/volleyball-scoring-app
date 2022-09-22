7a59a2104438cca5364bba64662f27f59c415b1d
Kamel BENDIMERAD <kamel.bendimerad@gmail.com>
2022-09-22 12:57:54 +0200

    inital setup

--- index.js
+++ index.js
@@ -3,22 +3,12 @@
 function App(){
 
      const [displayTime, setDisplayTime] = React.useState(0);
-    /*const [breakTime, setBreakTime] = React.useState(5*60);
-    const [sessionTime, setSessionTime] = React.useState(25*60);
-    const [timerOn, setTimerOn] = React.useState(false);
-    const [onBreak, setOnBreak] = React.useState(false);
-    const [breakAudio, setBreakAudio] = React.useState(new Audio("./breakTime.mp3"))
-    var executed = false;   // sound needs to run only once
+     const [teamPlayer1, setTeamPlayer1] = React.useState(["",""]);
+     const [teamPlayer2, setTeamPlayer2] = React.useState(["",""]);
+     const [setsWonTeam1, setSetsWonTeam1] = React.useState(0);
+     const [setsWonTeam2, setSetsWonTeam2] = React.useState(0);
+     const [setNumber, setSetNsetNumber] = React.useState([1,"1st"]);
     
-    var playBreakSound = (function(){
-        return function(){
-            if(!executed){
-                executed=true;
-                breakAudio.currentTime = 0;
-                breakAudio.play();
-            }
-        };
-    }) ();*/
     
 
     const formatTime = (time) => {
@@ -26,88 +16,52 @@ function App(){
         let seconds = time % 60; 
         return (minutes < 10 ? "0" + minutes : minutes) + " : " + (seconds < 10 ? "0" + seconds : seconds);
     }
-/*
-    const changeTime = (amount, type) => {
-        if (type == "break"){
-            if(breakTime <= 60){
-                return;
-            }
-            setBreakTime( prev => prev + amount);
-        }
-        else if (type =="session"){
-            if(sessionTime <= 60){
-                return;
-            }
-            setSessionTime( prev => prev + amount);
-            if(!timerOn){
-                setDisplayTime(displayTime + amount);
-            }
-        }
-    };
-    
-    const controlTime = () => {
-        let second = 1000;
-        let date = new Date().getTime();
-        let nextDate = new Date().getTime() + second;
-        let onBreakVariable = onBreak;
-        let indexSound = 0;
-        if(!timerOn){
-            let interval = setInterval( () => {
-                 date = new Date().getTime();
-                 if(date > nextDate){
-                    setDisplayTime(prev => {
-                        if( prev<=5 && !onBreakVariable){
-                            
-                                playBreakSound();
-                            
-                            
-                        }
-                        else if( prev <=5 && onBreakVariable){
-                            
-                            playBreakSound();
-                            
-                        }
-                        if( prev <= 0 && !onBreakVariable){
-                            indexSound++;
-                            onBreakVariable = true;
-                            setOnBreak(true);
-                            executed = false;
-                            return breakTime;
-                        }
-                        else if (prev <= 0 && onBreakVariable){
-                            indexSound++;
-                            onBreakVariable = false;
-                            setOnBreak(false);
-                            executed = false;
-                            return sessionTime;
-                        }
-                        return prev -1;})
-                    nextDate += second;
-                 };
-            }, 30)
-            localStorage.clear();
-            localStorage.setItem('interval-id', interval);
-        }
-        if(timerOn){
-           clearInterval(localStorage.getItem('interval-id')); 
-        }
 
-        setTimerOn(!timerOn);
-
-    };
-    const resetTime = () => {
-        setTimerOn(false);
-        setDisplayTime(25*60);
-        setBreakTime(5*60);
-        setSessionTime(25*60);
-    }; */
+    const changeNamePlayersTeam1 = index => e => {
+        let newArr = [...teamPlayer1];
+        newArr[index] = e.target.value;
+        setTeamPlayer1(newArr);
+    }
+    const changeNamePlayersTeam2 = index => e => {
+        let newArr = [...teamPlayer2];
+        newArr[index] = e.target.value;
+        setTeamPlayer2(newArr);
+    }
     return (
          <div className="center-align">
             <h3>Volleyball scoring Application</h3>
             <div className="dual-container">
-            <button type="button" class="btn btn-primary">Start</button>
-            <button type="button" class="btn btn-danger">End</button>
+                <div className="column">
+                    <p> <i className="material-icons">accessibility</i> Player 1 : </p>
+                    <input type="text" placeholder="player1" value={teamPlayer1[0]} onChange={changeNamePlayersTeam1(0)}></input>
+                    <p><i className="material-icons">accessibility</i> Player 2 : </p>
+                    <input placeholder="player2" value={teamPlayer1[1]} onChange={changeNamePlayersTeam1(1)}></input>
                 </div>
+                <div className="column">
+                    <p> <i className="material-icons">accessibility</i> Player 1 : </p>
+                    <input type="text" placeholder="player1" value={teamPlayer2[0]} onChange={changeNamePlayersTeam2(0)}></input>
+                    <p> <i className="material-icons">accessibility</i> Player 2 : </p>
+                    <input placeholder="player2" value={teamPlayer2[1]} onChange={changeNamePlayersTeam2(1)}></input>
+                </div>
+            </div>
+            <div className="container">
+                <div className="column team1">
+                    
+                    <p>Color : </p>
+                    <p>the team players are : {teamPlayer1[0].toUpperCase()} and {teamPlayer1[1].toUpperCase()}</p>
+                </div>
+                <div className="column middle">
+                    <p>{setNumber[1]} Set</p>
+                    <button>Start</button>
+                    <button>End</button>
+                </div>
+                <div className="column team2">
+                    
+                    <p>Color : </p>
+                    <p>the team players are : {teamPlayer2[0].toUpperCase()} and {teamPlayer2[1].toUpperCase()}</p>
+                
+                </div>
+            </div>
             {/* <Length title={"Break Length"} changeTime={changeTime} type={"break"} time={breakTime} formatTime={formatTime}/>
             <Length title={"Session Length"} changeTime={changeTime} type={"session"} time={sessionTime} formatTime={formatTime}/> */}
             {/* <h3>{onBreak ? "Break" : "Session"}</h3> */}
